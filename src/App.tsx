796c1c7518084b8807914bc7dda58c7a1fa25643
yplog <yalinpala@protonmail.com>
2022-07-28 11:10:10 +0300

    login form, set token, login logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import * as browser from 'webextension-polyfill';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as browser from "webextension-polyfill";
+import Login from "./components/Login";
 
 function App() {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
+  const [token, setToken] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLogin = async () => {
+    /*     fetch('localhost:3001', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: '',
+        password: ''
+      })
+    });
+ */
+    await browser.storage.local.set({ token: "token" });
+    let storageToken = await browser.storage.local.get("token");
+    console.log("storage token", storageToken);
+    //setToken(storageToken.token);
+  };
 
   useEffect(() => {
-    browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-      let tab = tabs[0];
-      setUrl(tab.url || '');
-    });
-  }, [])
+    const fetchData = async () => {
+      await handleLogin();
+    };
+
+    fetchData();
+
+    if (token) {
+      console.log("if token", token);
+      browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+        let tab = tabs[0];
+        setUrl(tab.url || "");
+      });
+      setIsLoading(false);
+    } else {
+      console.log("else token", token);
+      setIsLoading(false);
+    }
+  }, [token]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {url}
-        </p>
+        {isLoading ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : token ? (
+          <p>{url}</p>
+        ) : (
+          <Login setToken={() => setToken} />
+        )}
       </header>
     </div>
   );
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../logo.svg";
 
-export default function Login() {
+interface LoginProps {
+  setToken: () => void;
+}
+
+export default function Login({ setToken }: LoginProps) {
+  const [formData, setFormData] = useState({});
+
+  const handleOnChange = (e: any) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  }
+
+  const handleLogin = () => {
+    console.log('form data', formData);
+
+  }
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <img src={logo} className="App-logo" alt="logo" />
+      <input onChange={handleOnChange} placeholder="email" name="email" type="text" />
+      <input onChange={handleOnChange} placeholder="password" name="password" type="text" />
+      <button onClick={() => handleLogin()} className="button button1">login</button>
+    </>
+  );
 }
