3d3a1b6ad7d85fd3409eea2abf86ca3bf4eb1cf3
yplog <yalinpala@protonmail.com>
2022-08-31 13:28:16 +0300

    try catch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import libromarkologo from "./libromarko_logo.png";
 import "./App.css";
 import * as browser from "webextension-polyfill";
 import Login from "./components/Login";
 import Bookmark from "./components/Bookmark";
 
 const api = process.env.REACT_APP_API_URL;
-
 
 function App() {
   const [token, setToken] = useState("");
@@ -16,25 +15,34 @@ function App() {
     let storageToken = await browser.storage.local.get("token");
     console.log("storage token", storageToken);
     setToken(storageToken.token);
-    
+
     await userExists(storageToken.token);
   };
 
   const userExists = async (access_token: string) => {
-    const response = await fetch(`${api}user/me`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+    try {
+      const response = await fetch(`${api}user/me`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
 
-    const content = await response.json();
+      const content = await response.json();
 
-    console.log("content", content);
+      console.log("content", content);
 
-    if (content.statusCode === 500 || content.statusCode === 401) {
+      if (
+        content.statusCode === 500 ||
+        content.statusCode === 401 ||
+        content.statusCode === null
+      ) {
+        await browser.storage.local.remove("token");
+        setToken("");
+      }
+    } catch (error) {
       await browser.storage.local.remove("token");
       setToken("");
     }
@@ -54,7 +62,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {isLoading ? (
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={libromarkologo} style={{ height: "40vmin" }} alt="logo" />
         ) : token ? (
           <Bookmark token={token} />
         ) : (
